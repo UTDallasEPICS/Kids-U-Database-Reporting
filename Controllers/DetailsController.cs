7e4f628f66278db9c31635599c36abcd39e8da01
rakeen01 <42722928+rakeen01@users.noreply.github.com>
2019-10-27 17:37:35 -0500

    Add files via upload

---
+++ Controllers/DetailsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kids_U_Test.Controllers
+
+{
+    public class DetailsController : Controller
+    {
+        public ActionResult ShowOutcomeMeasurements()
+        {
+            return View();
+        }
+        public ActionResult ShowReportCardRecords()
+        {
+            return View();
+        }
+        public ActionResult ShowStudents()
+        {
+            return View();
+        }
+        public ActionResult ShowSchools()
+        {
+            return View();
+        }
+        public ActionResult ShowOrganizations()
+        {
+            return View();
+        }
+        public ActionResult ShowStaff()
+        {
+            return View();
+        }
+        public ActionResult ShowSites()
+        {
+            return View();
+        }
+        public ActionResult ShowSchoolDistricts()
+        {
+            return View();
+        }
+
+    }
+}
