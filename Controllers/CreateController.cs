73a41dceb8edf2eb7113c11e77225bd7b7bafc6b
dburgwin12 <55160282+dburgwin12@users.noreply.github.com>
2019-10-27 17:38:38 -0500

    Add files via upload

---
+++ Controllers/CreateController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class CreateController : Controller
+    {
+        public ActionResult CreateStudents()
+        {
+            return View ();
+        }
+        public ActionResult CreateOutcomeMeasurements()
+        {
+            return View();
+        }
+        public ActionResult CreateReportCards()
+        {
+            return View();
+        }
+        public ActionResult CreateStaff()
+        {
+            return View();
+        }
+        public ActionResult CreateSites()
+        {
+            return View();
+        }
+        public ActionResult CreateOrganization()
+        {
+            return View();
+        }
+        public ActionResult CreateSchoolDistricts()
+        {
+            return View();
+        }
+        public ActionResult CreateSchools()
+        {
+            return View();
+        }
+    }
+}
