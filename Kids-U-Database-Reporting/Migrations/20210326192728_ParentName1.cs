e66a2a1214fefdbef03cf3d63474452fb4f96f77
CS2337-DouglasCovington <72482149+CS2337-DouglasCovington@users.noreply.github.com>
2021-03-30 16:05:28 -0500

    added file that where intially ignored by git

---
+++ Kids-U-Database-Reporting/Migrations/20210326192728_ParentName1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kids_U_Database_Reporting.Migrations
+{
+    public partial class ParentName1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ParentName1",
+                table: "Students",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentName1",
+                table: "Students");
+        }
+    }
+}
